fae0e49ef16b9526bd6014f5512200985d2032c4
Kelvin Chao <kelvinchao2011@gmail.com>
2022-03-06 16:34:51 -0800

    added basic loading local storage

--- assets/js/script_basic.js
+++ assets/js/script_basic.js
@@ -8,9 +8,9 @@ var save_11am_btn = $('#save-11am')
 var save_12pm_btn = $('#save-12pm')
 var save_1pm_btn = $('#save-1pm')
 var save_2pm_btn = $('#save-2pm')
-var save_3pm_btn = $('#save-4pm')
-var save_4pm_btn = $('#save-5pm')
-var save_5pm_btn = $('#save-6pm')
+var save_3pm_btn = $('#save-3pm')
+var save_4pm_btn = $('#save-4pm')
+var save_5pm_btn = $('#save-5pm')
 
 
 
@@ -30,9 +30,31 @@ function today() {
 
 function setLocalStorage(input) {
 
-  localStorage.setItem('eventStorage', $('#text-9am').val())
+  // localStorage.setItem('eventStorage', $('#text-9am').val())
 }
 
+
+function getLocalStorage() {
+  var am_9 = localStorage.getItem('9am') || "";
+  var am_10 = localStorage.getItem('10am') || "";
+  var am_11 = localStorage.getItem('11am') || "";
+  var am_12 = localStorage.getItem('12pm') || "";
+  var am_1 = localStorage.getItem('1pm') || "";
+  var am_2 = localStorage.getItem('2pm') || "";
+  var am_3 = localStorage.getItem('3pm') || "";
+  var am_4 = localStorage.getItem('4pm') || "";
+  var am_5 = localStorage.getItem('5pm') || "";
+
+  $('#text-9am').val(am_9)
+  $('#text-10am').val(am_10)
+  $('#text-11am').val(am_11)
+  $('#text-12pm').val(am_12)
+  $('#text-1pm').val(am_1)
+  $('#text-2pm').val(am_2)
+  $('#text-3pm').val(am_3)
+  $('#text-4pm').val(am_4)
+  $('#text-5pm').val(am_5)
+}
 
 
 
@@ -44,6 +66,7 @@ function setLocalStorage(input) {
 function main() {
 
   today()
+  getLocalStorage()
 
   // eventlistners for each save icon
   save_9am_btn.on('click', () => {
@@ -73,7 +96,6 @@ function main() {
   save_5pm_btn.on('click', () => {
     localStorage.setItem('5pm', $('#text-5pm').val())
   })
-
 }
 
 main()
